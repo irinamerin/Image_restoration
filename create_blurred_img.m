7fde8cab763b618e7638715f44a1410fe2eae205
irinamerin <44139385+irinamerin@users.noreply.github.com>
2018-10-14 21:13:28 +0530

    Update create_blurred_img.m

--- create_blurred_img.m
+++ create_blurred_img.m
@@ -1,21 +1,21 @@
 clear all;close all;clc;
 
-f = imread('GroundTruth\GroundTruth1_1_1.jpg');
+f = imread('GroundTruth\GroundTruth1_1_1.jpg'); %load ground truth image
 [M,N,c]=size(f);
 figure,imshow(f);
 f=double(f);
 
-h = imread('Kernals\Ker1_3d.png');
+h = imread('Kernals\Ker1_3d.png');  %load kernel
 h=double(h);
 [A,B]=size(h);
 
-F=fft2(f);
+F=fft2(f);    %taking FFT
 H=fft2(h,M,N);
-Y=F.*H;
+Y=F.*H;        %Convoltion in time = Multiplication in frequency
 y1=real(ifft2(Y));
-y=mat2gray(y1);
-fi= uint8(y*255);
+y=mat2gray(y1);  %scaling to 0 to 1
+fi= uint8(y*255);  %scaling to 0-255 and displayin uint8 format
 figure,imshow(fi);
-imwrite(fi,'img_blurry.jpg','jpg');
+imwrite(fi,'img_blurry.jpg','jpg'); %saving the created blurred image
 
 
